5ef6fdd8bac87edbbc74731317cd6b36059bc645
Heidi Engstrom <hengstrom25@gmail.com>
2018-10-02 13:27:02 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,11 +10,11 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil
   end
   
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by(name: name)
+  def genre_id=(name)
+    self.genre = Genre.find_or_create_by(name: name)
   end
  
-  def artist_name
-    self.artist ? self.artist.name : nil
+  def genre_id
+    self.genre ? self.genre.id : nil
   end
 end
